d158dc8a65948b6b09faf9bfadac6f1d601e843b
Addison Cole <addison.cole85@gmail.com>
2019-01-04 23:00:35 -0800

    add path to urls.py for index view and to link route HomelessCounts to seattlizationAPI app. Create simlple hello world in view of seattlizationAPI app at index.

--- api/urls.py
+++ api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('seattlizationAPI.urls'))
+    path('homelesscounts/', include('seattlizationAPI.urls'))
 ]
